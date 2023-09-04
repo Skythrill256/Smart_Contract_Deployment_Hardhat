878f0516f4bdc052c5096e2b323b4f9d467af252
Anjishnu Ganguly <113181374+Skythrill256@users.noreply.github.com>
2023-09-04 08:46:33 +0530

    Update SimpleStorage.sol

--- contracts/SimpleStorage.sol
+++ contracts/SimpleStorage.sol
@@ -1,11 +1,5 @@
-// I'm a comment!
 // SPDX-License-Identifier: MIT
-
 pragma solidity 0.8.19;
-
-// pragma solidity ^0.8.0;
-// pragma solidity >=0.8.0 <0.9.0;
-
 contract SimpleStorage {
   uint256 favoriteNumber;
 
